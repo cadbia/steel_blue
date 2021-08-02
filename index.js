a979df74daee9995af283f65d17c6ad3d78c81b6
Drakomire <Bambolambo0@gmail.com>
2021-08-02 11:41:50 -0400

    i really should have checked the code again...

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Query by City ID instead of City name to avoid funky stuff with URL encoding.
 //City ID can be found in city.list.json
 
-function fetchCity(cityID,callback){
+function fetchCity(cityId,callback){
     fetch(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=939aef0ebf0cecd4d85905f7f983915d`)
     .then(response => response.json())
     .then(data => callback(data));
