b282d340247ef5a50cba40a4942161520a2692f2
Bhadra Rupesh <bhadrarupesh@MacBook-Pro-2.local>
2021-08-03 14:24:20 -0700

    bracket missing

--- src/util.js
+++ src/util.js
@@ -1,47 +1,47 @@
-class Coordinate{
-    constructor(lat,long){
+class Coordinate {
+    constructor(lat, long) {
         this.latitude = lat
         this.longitude = long
     }
 
-    static fromCoordinates(coords){
+    static fromCoordinates(coords) {
         let lat = coords.latitude;
         let long = coords.longitude;
-        return new Coordinate(lat,long)
+        return new Coordinate(lat, long)
     }
 }
 
 const TEMP = {
-    KELVIN : 0,
-    FAHRENHEIT : 1,
-    CELSIUS : 2,
+    KELVIN: 0,
+    FAHRENHEIT: 1,
+    CELSIUS: 2,
 }
 
-function convertTempetureUnit(degree,inType,outType,decimals = 2) {
+function convertTempetureUnit(degree, inType, outType, decimals = 2) {
     //Convert temp to Kelvin
-    switch (inType){
+    switch (inType) {
         case TEMP.KELVIN:
-            
+
             break
         case TEMP.FAHRENHEIT:
-            degree = (degree-32) * 5/9 + 273.15
+            degree = (degree - 32) * 5 / 9 + 273.15
             break
         case TEMP.CELSIUS:
-            degree = (degree+273.15)
+            degree = (degree + 273.15)
             break
         default:
             throw "Tempeture type is not valid"
     }
 
-    switch (outType){
+    switch (outType) {
         case TEMP.KELVIN:
-            
+
             break
         case TEMP.FAHRENHEIT:
-            degree = (degree - 273.15) * 9/5 + 32
+            degree = (degree - 273.15) * 9 / 5 + 32
             break
         case TEMP.CELSIUS:
-            degree = (degree-273.15)
+            degree = (degree - 273.15)
             break
         default:
             throw "Tempeture type is not valid"
@@ -53,8 +53,9 @@ function convertTempetureUnit(degree,inType,outType,decimals = 2) {
 
 function toTitleCase(str) {
     return str.replace(
-      /\w\S*/g,
-      function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-      }
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
     );
+}
