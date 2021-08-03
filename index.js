835c0f31f14e77d54102ca7c3b55faa95a5efab2
cadbia <cadenbialow@icloud.com>
2021-08-03 11:23:26 -0400

    temp change funtion

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 //Query by City ID instead of City name to avoid funky stuff with URL encoding.
 //City ID can be found in city.list.json
 
+
 const weatherAPI = new WeatherAPI("939aef0ebf0cecd4d85905f7f983915d")
+const city = document.querySelector(".location-city");
+const temperatureSection = document.querySelector(".temperature");
+const degreeType = document.querySelector(".temperature h1");
+const degree = document.querySelector(".temperature h2");
 
 window.addEventListener("load", ()=>{
     if(navigator.geolocation){
@@ -10,6 +15,18 @@ window.addEventListener("load", ()=>{
                 writeWeatherInfo(data)
             })
         });
+    }
+});
+
+temperatureSection.addEventListener("click", ()=>{
+    if(degreeType.textContent == "F"){
+        degreeType.textContent = "C";
+        degree.textContent =  ((degree.textContent-32) *(5/9)).toFixed(2);
+    }
+    else
+    {
+        degreeType.textContent = "F"
+        degree.textContent =  ((degree.textContent*(9/5)) +32).toFixed(2);
     }
 });
 
