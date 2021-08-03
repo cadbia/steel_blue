7dc735d17f1753740c04f705c903bbfadc2c1536
Bhadra Rupesh <bhadrarupesh@MacBook-Pro-2.local>
2021-08-03 08:43:20 -0700

    reverted the previous skycon

--- index.js
+++ index.js
@@ -12,39 +12,6 @@ window.addEventListener("load", ()=>{
         });
     }
 
-    let dropdown = document.getElementById("city-dropdown");
-
-    let defaultOption = document.createElement('option');
-    defaultOption.textContent = 'Choose City';
-
-    dropdown.appendChild(defaultOption);
-    dropdown.selectedIndex = 0;
-
-    const url = 'city.list.json';
-
-    const request = new XMLHttpRequest();
-    request.open('GET', url, true);
-
-    request.onload = function () {
-        if (request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            let option;
-            for (let i = 0; i < data.length; i++) {
-                option = document.createElement('option');
-                option.textContent = data[i].name;
-                option.value = data[i].id;
-                dropdown.appendChild(option);
-            }
-        } else {
-            // Reached the server, but it returned an error
-        }
-    }
-
-    request.onerror = function () {
-        console.error('An error occurred fetching the JSON from ' + url);
-    };
-
-    request.send();
 });
 
 WEATHER_TYPES = {
