8f91cf4716630791ee8830389ca5fb2b28096e2a
Bhadra Rupesh <bhadrarupesh@MacBook-Pro-2.local>
2021-08-03 09:05:36 -0700

    merged main

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 //Query by City ID instead of City name to avoid funky stuff with URL encoding.
 //City ID can be found in city.list.json
-
+const feelsLike = document.querySelector("#weatherDescription");
 const weatherAPI = new WeatherAPI("939aef0ebf0cecd4d85905f7f983915d")
+const city = document.querySelector(".location-city");
+const temperatureSection = document.querySelector(".temperature");
+const degreeType = document.querySelector(".temperature h1");
+const h2degree = document.querySelector(".temperature h2");
 
 window.addEventListener("load", ()=>{
     if(navigator.geolocation){
@@ -11,7 +15,6 @@ window.addEventListener("load", ()=>{
             })
         });
     }
-
 });
 
 WEATHER_TYPES = {
@@ -35,12 +38,37 @@ WEATHER_TYPES = {
     "Tornado": "icon",
 
 }
+function numberExtract(str) { 
 
+    var matches = str.match(/[\d\.]+/); 
+    if (matches) { 
+        return matches[0]; 
+    } 
+} 
+
+temperatureSection.addEventListener("click", ()=>{
+    if(degreeType.textContent == "°F"){
+        degreeType.textContent = "°C";
+        h2degree.textContent =  ((h2degree.textContent-32) *(5/9)).toFixed(2);
+        let feelNum = numberExtract(feelsLike.textContent);
+        
+        feelsLike.textContent = "Feels like " + ((feelNum-32) *(5/9)).toFixed(2);
+    }
+    else
+    {
+        degreeType.textContent = "°F"
+        h2degree.textContent =  ((h2degree.textContent*(9/5)) +32).toFixed(2);
+        let feelNum = numberExtract(feelsLike.textContent);
+        feelsLike.textContent =  "Feels like " + ((feelNum*(9/5)) +32).toFixed(2);
+    }
+});
 var skycons = new Skycons({ "color": "white" });
-
 function writeWeatherInfo(data) {
-    document.getElementById("degree").innerHTML = convertTempetureUnit(data.main.temp,TEMP.KELVIN,TEMP.FAHRENHEIT)
-    document.getElementById("weatherDescription").innerHTML = data.weather[0].main
+    console.log(data)
+    document.getElementById("degree").innerHTML = `${convertTempetureUnit(data.main.temp,TEMP.KELVIN,TEMP.FAHRENHEIT)}`
+    document.getElementById("weatherDescription").innerHTML = `Feels like ${convertTempetureUnit(data.main.feels_like,TEMP.KELVIN,TEMP.FAHRENHEIT)}`
+    document.getElementById("tempDescription").innerHTML = toTitleCase(data.weather[0].description)
+    document.getElementById("locationCity").innerHTML = `${data.name}, ${data.sys.country}`
     var skyconElement = document.getElementsByClassName("icon")[0]
     var weatherIconName = WEATHER_TYPES[data.weather[0].main]
     skycons.set(skycon, weatherIconName)
