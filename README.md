0b15673975bbb8e2100941fc3e4b47a0613ac6c9
JDodoo16 <jeremydodoo20@gmail.com>
2021-08-04 09:11:41 -0400

    Background Color Animation

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # steel_blue
-GitHub for Columbia HS Coding Camp
+Jeremy Dodoo
+
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+    font-family: sans-serif;
+    color:white
+}
+@keyframes gradient {
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+}
---
+++ index.js
@@ -0,0 +1,81 @@
+//Query by City ID instead of City name to avoid funky stuff with URL encoding.
+//City ID can be found in city.list.json
+const feelsLike = document.querySelector("#weatherDescription");
+const weatherAPI = new WeatherAPI("939aef0ebf0cecd4d85905f7f983915d")
+const city = document.querySelector(".location-city");
+const temperatureSection = document.querySelector(".temperature");
+const degreeType = document.querySelector(".temperature h1");
+const h2degree = document.querySelector(".temperature h2");
+
+window.addEventListener("load", () => {
+    console.log("preload")
+    document.all["layer1"].style.visibility = "visible";
+    document.all["layer2"].style.visibility = "hidden";
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            weatherAPI.fetchDataByCoordinate(Coordinate.fromCoordinates(position.coords), data => {
+                writeWeatherInfo(data)
+            })
+        });
+    }
+});
+
+WEATHER_TYPES = {
+    "Thunderstorm": "FOG",
+    "Drizzle": "RAIN",
+    "Rain": "SLEET",
+    "Snow": "SNOW",
+    "Clear": "CLEAR-DAY",
+    "Clouds": "PARTLY_CLOUDY_DAY",
+
+    //These are the 7xx ones. They are for atmosphere weather, not sure if they can be grouped.
+    "Mist": "icon",
+    "Smoke": "icon",
+    "Haze": "icon",
+    "Dust": "icon",
+    "Fog": "CLOUDY",
+    "Sand": "icon",
+    "Dust": "icon",
+    "Ash": "icon",
+    "Squall": "icon",
+    "Tornado": "icon",
+
+}
+function numberExtract(str) {
+
+    var matches = str.match(/[\d\.]+/);
+    if (matches) {
+        return matches[0];
+    }
+}
+
+temperatureSection.addEventListener("click", () => {
+    if (degreeType.textContent == "°F") {
+        degreeType.textContent = "°C";
+        h2degree.textContent = ((h2degree.textContent - 32) * (5 / 9)).toFixed(2);
+        let feelNum = numberExtract(feelsLike.textContent);
+
+        feelsLike.textContent = "Feels like " + ((feelNum - 32) * (5 / 9)).toFixed(2);
+    }
+    else {
+        degreeType.textContent = "°F"
+        h2degree.textContent = ((h2degree.textContent * (9 / 5)) + 32).toFixed(2);
+        let feelNum = numberExtract(feelsLike.textContent);
+        feelsLike.textContent = "Feels like " + ((feelNum * (9 / 5)) + 32).toFixed(2);
+    }
+});
+
+var skycons = new Skycons({ "color": "white" });
+function writeWeatherInfo(data) {
+    node = document.getElementById("layer1").style.visibility = 'hidden';
+    node = document.getElementById("layer2").style.visibility = 'visible';
+    console.log(data)
+    document.getElementById("degree").innerHTML = `${convertTempetureUnit(data.main.temp, TEMP.KELVIN, TEMP.FAHRENHEIT)}`
+    document.getElementById("weatherDescription").innerHTML = `Feels like ${convertTempetureUnit(data.main.feels_like, TEMP.KELVIN, TEMP.FAHRENHEIT)}`
+    document.getElementById("tempDescription").innerHTML = toTitleCase(data.weather[0].description)
+    document.getElementById("locationCity").innerHTML = `${data.name}, ${data.sys.country}`
+    var skyconElement = document.getElementsByClassName("icon")[0]
+    var weatherIconName = WEATHER_TYPES[data.weather[0].main]
+    skycons.set(skycon, weatherIconName)
+    skycons.play()
+}
