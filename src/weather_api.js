dd8ab727c827df04916411bb458c56085d4d75fc
Drakomire <Bambolambo0@gmail.com>
2021-08-04 13:30:54 -0400

    update to nginx

--- src/weather_api.js
+++ src/weather_api.js
@@ -4,31 +4,31 @@ class WeatherAPI{
     }
 
     fetchDataByCityId(cityId,callback){
-        fetch(`http://steelblueweatherapp.duckdns.org:4682/api/weather?id=${cityId}`)
+        fetch(`https://steelblueweatherapp.duckdns.org:4682/api/weather?id=${cityId}`)
         .then(response => response.json())
         .then(data => callback(data));
     }
 
     fetchDataByCityName(city,callback){
-        fetch(`http://steelblueweatherapp.duckdns.org:4682/api/weather?q=${city}`)
+        fetch(`https://steelblueweatherapp.duckdns.org:4682/api/weather?q=${city}`)
         .then(response => response.json())
         .then(data => callback(data));
     }
 
     fetchDataByCoordinate(coordinate,callback){
-        fetch(`http://steelblueweatherapp.duckdns.org:4682/api/weather?lat=${coordinate.latitude}&lon=${coordinate.longitude}`)
+        fetch(`https://steelblueweatherapp.duckdns.org:4682/api/weather?lat=${coordinate.latitude}&lon=${coordinate.longitude}`)
         .then(response => response.json())
         .then(data => callback(data));
     }
 
     fetchWeeklyForcastByCityName(city,callback){
-        fetch(`http://steelblueweatherapp.duckdns.org:4682/api/forecast/daily?q=${city}&cnt=7`)
+        fetch(`https://steelblueweatherapp.duckdns.org:4682/api/forecast/daily?q=${city}&cnt=7`)
         .then(response => response.json())
         .then(data => callback(data));
     }
 
     fetchWeeklyForcastByCoordinate(coordinate,callback){
-        fetch(`http://steelblueweatherapp.duckdns.org:4682/api/forecast/daily?lat=${coordinate.latitude}&lon=${coordinate.longitude}&cnt=7}`)
+        fetch(`https://steelblueweatherapp.duckdns.org:4682/api/forecast/daily?lat=${coordinate.latitude}&lon=${coordinate.longitude}&cnt=7}`)
         .then(response => response.json())
         .then(data => callback(data));
     }
